9e0d1ae7410dcd9e7880dbbdd1be61d0e8ed3be7
Ahmet Sofuoğlu <ahmetresatsofuoglu@Ahmet-MacBook-Pro.local>
2024-09-05 11:26:23 +0300

    updated taskorders

--- taskorders.js
+++ taskorders.js
@@ -22,19 +22,9 @@ for(let i=0;i<customers.length;i++){
     let totalprice=0
     let orders=[]
     for(let j=0;j<customers[i].orders.length;j++){
-       
-        if(customers[i].orders[j].product==='Burger'){
-        totalprice+=5*(customers[i].orders[j].quantity)
-       }
-       if(customers[i].orders[j].product==='Salad'){
-        totalprice+=4*(customers[i].orders[j].quantity)
-       }
-       if(customers[i].orders[j].product==='Fries'){
-        totalprice+=3*(customers[i].orders[j].quantity)
-       }
-       
+       totalprice+=(customers[i].orders[j].quantity)*(customers[i].orders[j].price)
     }
-    
+
     for(let k=0;k<customers[i].orders.length;k++){
        
         orders[k]=[customers[i].orders[k].quantity+ 'x '+customers[i].orders[k].product+'']
