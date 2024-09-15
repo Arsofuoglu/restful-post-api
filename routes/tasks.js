be41bd36e964f02993ff6bc402cbc9b66b475909
Ahmet Sofuoglu <ahmetresatsofuoglu@gmail.com>
2024-09-15 18:42:41 +0300

    updated post service

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const {findMostLikeUser,findMostCommentUser,newPost, posts, user, finalExport } = require('../tasks/task6_posts');
+const { getFinalExport,updateLikeComments,newPost, posts, user,} = require('../tasks/task6_posts');
 const orders = require('../tasks/task5_orders');
 const bubbleCities = require('../tasks/task4_bubble')
 const selectionCities = require('../tasks/task3_selection')
@@ -10,9 +10,9 @@ const departmentLists = require('../tasks/task1_reduce')
 
 router.use(express.json()); // JSON verilerini parse etmek için gerekli(jsonu programın anlayacağı hale getir)
 
-let newUserId = user.length
-router.post("/post", (req, res, next) => {
 
+router.post("/post", (req, res, next) => {
+    let newUserId = user.length
     for (let i = 0; i < req.body.user.length; i++) {
         let newUser = req.body.user[i]
         let newKey = `user${newUserId}`;
@@ -21,35 +21,38 @@ router.post("/post", (req, res, next) => {
         newUserId++
     }
     for (let i = 0; i < req.body.user.length; i++) {
-        let newPosts = newPost(); 
+        let newPosts = newPost();
         posts.push(newPosts);
     }
+    updateLikeComments()
     res.json(user)
     next()
 })
 
 router.get("/:id", (req, res) => {
-    switch (Number(req.params.id)){
-    case 1:
-        return res.send(departmentLists);
-    case 2:
-        return res.send(popListWithSort);  
-    case 3:
-        return res.send(selectionCities);  
-    case 4:
-        return res.send(bubbleCities);
-    case 5:
-        return res.send(orders);
-    case 6:
-        return res.json(finalExport);
-    case 7:
-        return res.send(user);
-    case 8:
-        return res.send(posts);
-    default:
-        return res.send("Invalid Id");
+    switch (Number(req.params.id)) {
+        case 8:
+            return res.send(posts);
+        case 7:
+            return res.send(user);
+        case 6:
+            return res.send(getFinalExport());
+        case 5:
+            return res.send(orders);
+        case 4:
+            return res.send(bubbleCities);
+        case 3:
+            return res.send(selectionCities);
+        case 2:
+            return res.send(popListWithSort);
+        case 1:
+            return res.send(departmentLists);
+        default:
+            return res.send("Invalid Id");
+
+
     }
-    }
+}
 )
 
 
--- tasks/task6_posts.js
+++ tasks/task6_posts.js
@@ -57,24 +57,34 @@ for (let i = 0; i < maxUser; i++) {
 // 3.adım bu oluşturulan post array içinde en çok beğeni yapan ve en çok yorum yapan kullanıcıları bulacaz.
 const allLikes = []
 const allComments = []
-for (let i = 0; i < posts.length; i++) {
-    for (let j = 0; j < posts[i].comments.length+posts[i].likes.length; j++) {
-        if(posts[i].comments[j]!==undefined){
-            allComments.push(posts[i].comments[j].user)
+const updateLikeComments = () => {
+    allLikes.length = 0
+    allComments.length = 0
+    for (let i = 0; i < posts.length; i++) {
+        for (let j = 0; j < posts[i].comments.length + posts[i].likes.length; j++) {
+            if (posts[i].comments[j] !== undefined) {
+                allComments.push(posts[i].comments[j].user)
+            }
+
+            if (posts[i].likes[j] !== undefined) {
+                allLikes.push(posts[i].likes[j])
+            }
+
         }
-        
-        if(posts[i].likes[j]!==undefined){
-            allLikes.push(posts[i].likes[j])
-        }
-        
     }
 }
+updateLikeComments()
 const findMostCommentUser = () => {
     let mostCommenteds = 0
     let mostCommentingUser = null
 
     for (let i = 0; i < allComments.length; i++) {
         let commentAmount = 1
+        if (allComments.length === 1) {
+            mostCommenteds = commentAmount
+            mostCommentingUser = allLikes[i][0]
+            return `${mostCommentingUser} with ${mostCommenteds} times `
+        }
         for (let j = i + 1; j < allComments.length; j++) {
 
             if (allComments[i][0] === allComments[j][0]) {
@@ -83,15 +93,15 @@ const findMostCommentUser = () => {
             if (mostCommenteds < commentAmount) {
                 mostCommenteds = commentAmount
                 mostCommentingUser = allComments[i][0]
-            }         
-            if (mostCommenteds==commentAmount && allComments[i][0]<mostCommentingUser){
-                mostCommentingUser=allComments[i][0]
+            }
+            else if (mostCommenteds == commentAmount && allComments[i][0] < mostCommentingUser) {
+                mostCommentingUser = allComments[i][0]
 
             }
         }
     }
-    
-return `${mostCommentingUser} with ${mostCommenteds} times `
+
+    return `${mostCommentingUser} with ${mostCommenteds} times `
 }
 const findMostLikeUser = () => {
     let mostLikes = 0
@@ -99,6 +109,11 @@ const findMostLikeUser = () => {
 
     for (let i = 0; i < allLikes.length; i++) {
         let likesAmount = 1
+        if (allLikes.length === 1) {
+            mostLikes = likesAmount
+            mostLikingUser = allLikes[i][0]
+        }
+
         for (let j = i + 1; j < allLikes.length; j++) {
 
             if (allLikes[i][0] === allLikes[j][0]) {
@@ -108,22 +123,26 @@ const findMostLikeUser = () => {
                 mostLikes = likesAmount
                 mostLikingUser = allLikes[i][0]
             }
-            if (mostLikes==likesAmount && allLikes[i][0]<mostLikingUser){
-                mostLikingUser=allLikes[i][0]
+            if (mostLikes == likesAmount && allLikes[i][0] < mostLikingUser) {
+                mostLikingUser = allLikes[i][0]
 
             }
         }
-
     }
+
     return `${mostLikingUser} with ${mostLikes} times `
 }
 
-const finalExport = [`Most Liking User: ${findMostLikeUser()} 
-Most Commenting User: ${findMostCommentUser()}`]
 
-module.exports = {findMostLikeUser,findMostCommentUser,newPost,posts,user,finalExport}
+const getFinalExport = () => {
+    return `Most Liking User: ${findMostLikeUser()} \nMost Commenting User: ${findMostCommentUser()}`;
+}
+
+
+
+module.exports = {getFinalExport,updateLikeComments, findMostLikeUser, findMostCommentUser, newPost, posts, user}
 // console.log(posts)
 // console.log(allLikes)
 // console.log(allComments)
 // console.log(finalExport)
-//Posts verisine post servisi ile ekleme yapıp.body de gönder.tekrar get ile çağır.
+//Posts verisine post servisi ile ekleme yapıp body de gönder tekrar get ile çağır.
